bd53496a17cfb5c04499e83971e065b22c22bf4c
Haoran Chen <chenhaoran.earth@live.com>
2019-12-02 16:07:58 +0800

    add CIFAR100 part

---
+++ CIFAR100/FMPnet.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+'''
+Author: Haoran Chen
+Date: 2018-05-02
+'''
+
+
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+
+NUM_FILTERS = 64
+CONV_SIZE = 2
+NUM_CHANNELS = 3
+NUM_OUTPUT = 100
+RATIO = [1.0, 2**(1/3), 2**(1/3), 1.0]
+PSEUDO_RANDOM = True
+OVERLAPPING = True
+
+
+def single_conv_layer(input_tensor, num_filters, pool_flag, name):
+    
+    with tf.variable_scope(name):
+        conv = layers.Conv2D(num_filters, 2, activation=tf.nn.relu)
+        x = conv(input_tensor)
+        if pool_flag:
+            x, row_sq, col_sq = tf.nn.fractional_max_pool(
+                    x, RATIO, PSEUDO_RANDOM, OVERLAPPING)
+        print(x.name, x.get_shape())
+    return x
+
+
+def inference(input_tensor):
+    x = single_conv_layer(input_tensor, NUM_FILTERS, True, 'conv1')
+    x = single_conv_layer(x, NUM_FILTERS*2, True, 'conv2')
+    x = single_conv_layer(x, NUM_FILTERS*3, True, 'conv3')
+    x = single_conv_layer(x, NUM_FILTERS*4, True, 'conv4')
+    x = single_conv_layer(x, NUM_FILTERS*5, True, 'conv5')
+    x = single_conv_layer(x, NUM_FILTERS*6, True, 'conv6')
+    x = single_conv_layer(x, NUM_FILTERS*7, True, 'conv7')
+    x = single_conv_layer(x, NUM_FILTERS*8, True, 'conv8')
+    x = single_conv_layer(x, NUM_FILTERS*9, True, 'conv9')
+    x = single_conv_layer(x, NUM_FILTERS*10, True, 'conv10')
+    x = single_conv_layer(x, NUM_FILTERS*11, True, 'conv11')
+    x = single_conv_layer(x, NUM_FILTERS*12, True, 'conv12')
+    x = single_conv_layer(x, NUM_FILTERS*6, False, 'conv13')
+    conv = layers.Conv2D(10, 1)
+    x = conv(x) 
+    x = tf.squeeze(x)
+    return x
+
---
+++ CIFAR100/preprocess.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+'''
+Author: Haoran Chen
+Date: 2018-04-18
+'''
+
+import numpy as np
+import os
+from PIL import Image
+import tensorflow.keras.datasets.cifar100 as cifar100
+
+
+data = cifar100.load_data()
+train_dev_data, test_data = data[0], data[1]
+train_dev_samples, train_dev_labels = train_dev_data
+print('CIFAR100 train dev samples:', train_dev_samples.shape)
+test_samples, test_labels = test_data
+print('CIFAR100 test samples:', test_samples.shape)
+
+class get_data():
+    def __init__(self, flag, image_size):
+        self.flag = flag
+        self.image_size = image_size
+        if flag == "train":
+            # images shape: (45000, 32, 32, 1), labels shape: (45000,)
+            self.images, self.labels = train_dev_samples[:-5000], train_dev_labels[:-5000]
+
+        elif flag == "dev":
+            # images shape: (5000, 32, 32, 1), labels shape: (5000,)
+            self.images, self.labels = train_dev_samples[-5000:], train_dev_labels[-5000:]
+        
+        elif flag == "test":
+            # images shape: (10000, 32, 32, 1), labels shape: (10000,)
+            self.images, self.labels = test_samples, test_labels
+
+        else:
+            raise ValueError
+
+        tmp = []
+        for img in self.images:
+            img_resize = Image.fromarray(img).resize((image_size, image_size))
+            tmp.append(img_resize)
+        tmp = np.stack(tmp, axis=0)
+        tmp = np.expand_dims(tmp, axis=-1)
+        print('Resized images:', tmp.shape)
+        self.images = tmp
+        self.images = self.images.astype(np.float32)
+        # normalize the images
+        self.images = (self.images / 256 - 0.5) * 2
+        self.start_index = 0
+        self.length = self.images.shape[0]
+        self.indices = np.arange(self.length)
+
+    def shuffle(self):
+        np.random.shuffle(self.indices) 
+    
+    def get_next_batch(self, size, if_arg):
+        end = min(self.start_index + size, self.length)
+        size = end - self.start_index
+
+        ret_images = self.images[self.indices[self.start_index:end]]
+        ret_labels = self.labels[self.indices[self.start_index:end]]
+        if if_arg:
+            for i in range(size):
+                if_need_to_flip = np.random.choice([True, False])
+                if if_need_to_flip:
+                    ret_images[i] = np.fliplr(ret_images[i])
+                                            
+        self.start_index = end
+        return ret_images, ret_labels
+
